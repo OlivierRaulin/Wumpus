f2f22289ee5087cb2e3f9ae862c1f663379095c1
Olivier Raulin <olivier@olivier-raulin.fr>
2013-11-27 16:10:51 +0100

    Debug client

--- Client.java
+++ Client.java
@@ -14,11 +14,25 @@ public class Client {
 				{
 				Thread.sleep(0);
 				PrintWriter socketOut = new PrintWriter(socket.getOutputStream(), true);
-				//BufferedReader clavier = new BufferedReader(new InputStreamReader(System.in));
+				BufferedReader clavier = new BufferedReader(new InputStreamReader(System.in));
+				BufferedReader socketIn = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+				
 				//System.out.print("Message : ");
-				//String message = clavier.readLine(); 
-				//socketOut.println(message);
-				Frame frame = new Frame("Client Wumpus");
+				while(true)
+				{
+					while(!socketIn.ready())
+					{
+						Thread.sleep(0);					
+					}
+					while(socketIn.ready())
+					{
+						System.out.println(socketIn.readLine());
+						Thread.sleep(100);
+					}
+					String message = clavier.readLine(); 
+					socketOut.println(message);
+				}
+				//Frame frame = new Frame("Client Wumpus");
  				} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
